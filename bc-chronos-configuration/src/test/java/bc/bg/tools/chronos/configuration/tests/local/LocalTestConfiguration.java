9cda00f35081d26427df55f5f303ca250c8fe89d
neokaozzz@gmail.com <neokaozzz@gmail.com>
2017-05-20 14:59:48 +0300

    Append previous commit - redundant configuration

--- bc-chronos-configuration/src/test/java/bc/bg/tools/chronos/configuration/tests/local/LocalTestConfiguration.java
+++ bc-chronos-configuration/src/test/java/bc/bg/tools/chronos/configuration/tests/local/LocalTestConfiguration.java
@@ -2,10 +2,9 @@ package bc.bg.tools.chronos.configuration.tests.local;
 
 import org.springframework.context.annotation.Import;
 
-import bc.bg.tools.chronos.configuration.I18nConfig;
 import bc.bg.tools.chronos.configuration.LocalDBConfig;
 import bc.bg.tools.chronos.configuration.LocalDataProviderConfig;
 
-@Import(value = { LocalDBConfig.class, LocalDataProviderConfig.class, I18nConfig.class })
+@Import(value = { LocalDBConfig.class, LocalDataProviderConfig.class })
 public class LocalTestConfiguration {
 }
--- bc-chronos-configuration/src/test/java/bc/bg/tools/chronos/configuration/tests/remote/RemoteTestConfiguration.java
+++ bc-chronos-configuration/src/test/java/bc/bg/tools/chronos/configuration/tests/remote/RemoteTestConfiguration.java
@@ -2,10 +2,9 @@ package bc.bg.tools.chronos.configuration.tests.remote;
 
 import org.springframework.context.annotation.Import;
 
-import bc.bg.tools.chronos.configuration.I18nConfig;
 import bc.bg.tools.chronos.configuration.RemoteDBConfig;
 import bc.bg.tools.chronos.configuration.RemoteDataProviderConfig;
 
-@Import(value = { RemoteDBConfig.class, RemoteDataProviderConfig.class, I18nConfig.class })
+@Import(value = { RemoteDBConfig.class, RemoteDataProviderConfig.class })
 public class RemoteTestConfiguration {
 }
