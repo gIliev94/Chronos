abc1cadaac3d06e8a158590c59e4bf057ed20b74
neokaozzz@gmail.com <neokaozzz@gmail.com>
2017-06-11 20:48:57 +0300

    Append tree view test code

--- bc-chronos-endpoint/src/main/java/bc/bg/tools/chronos/endpoint/ui/main/MainViewController.java
+++ bc-chronos-endpoint/src/main/java/bc/bg/tools/chronos/endpoint/ui/main/MainViewController.java
@@ -13,17 +13,26 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import bg.bc.tools.chronos.core.entities.DCategory;
 import bg.bc.tools.chronos.core.entities.DCustomer;
 import bg.bc.tools.chronos.core.entities.DObject;
+import bg.bc.tools.chronos.core.entities.DProject;
+import bg.bc.tools.chronos.core.entities.DRole;
+import bg.bc.tools.chronos.core.entities.DTask;
 import bg.bc.tools.chronos.dataprovider.db.local.services.ifc.ILocalCategoryService;
 import bg.bc.tools.chronos.dataprovider.db.local.services.ifc.ILocalCustomerService;
 import bg.bc.tools.chronos.dataprovider.db.local.services.ifc.ILocalProjectService;
 import bg.bc.tools.chronos.dataprovider.db.local.services.ifc.ILocalRoleService;
 import bg.bc.tools.chronos.dataprovider.db.local.services.ifc.ILocalTaskService;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -41,6 +50,9 @@ public class MainViewController implements Initializable {
 
     @FXML
     private TreeView<DObject> treeRoles;
+
+    @FXML
+    private VBox stackEntityAttributes;
 
     @Autowired
     @Qualifier("localCategoryService")
@@ -148,6 +160,36 @@ public class MainViewController implements Initializable {
 			.map(TreeItem<DObject>::new) // nl
 			.forEach(catNode.getChildren()::add);
 	    }
+	}
+
+	tree.getSelectionModel().selectedItemProperty().addListener(this::showEntityAttributes);
+    }
+
+    private void showEntityAttributes(ObservableValue<? extends TreeItem<DObject>> observable,
+	    TreeItem<DObject> old_val, TreeItem<DObject> new_val) {
+	final ObservableList<Node> entityAttrList = stackEntityAttributes.getChildren();
+	entityAttrList.clear();
+
+	final DObject newValueObj = new_val.getValue();
+	if (newValueObj instanceof DCustomer) {
+	    DCustomer custObj = (DCustomer) newValueObj;
+	    entityAttrList.add(new HBox(new Label("Name: "), new TextField(custObj.getName())));
+	    entityAttrList.add(new HBox(new Label("Description: "), new TextField(custObj.getDescription())));
+	} else if (newValueObj instanceof DProject) {
+	    DProject projObj = (DProject) newValueObj;
+	    entityAttrList.add(new HBox(new Label("Name: "), new TextField(projObj.getName())));
+	    entityAttrList.add(new HBox(new Label("Description: "), new TextField(projObj.getDescription())));
+	} else if (newValueObj instanceof DTask) {
+	    DTask taskObj = (DTask) newValueObj;
+	    entityAttrList.add(new HBox(new Label("Name: "), new TextField(taskObj.getName())));
+	    entityAttrList.add(new HBox(new Label("Description: "), new TextField(taskObj.getDescription())));
+	    entityAttrList.add(new HBox(new Label("Hours estimated: "),
+		    new TextField(String.valueOf(taskObj.getHoursEstimated()))));
+	} else if (newValueObj instanceof DRole) {
+	    DRole roleObj = (DRole) newValueObj;
+	    entityAttrList.add(new HBox(new Label("Name: "), new TextField(roleObj.getName())));
+	    entityAttrList.add(new HBox(new Label("Description: "), new TextField(roleObj.getDescription())));
+	    entityAttrList.add(new HBox(new Label("Rate: "), new TextField(String.valueOf(roleObj.getBillingRate()))));
 	}
     }
 }
